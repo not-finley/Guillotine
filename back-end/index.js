d89fa27a083dbb5557eceb48fedb7d48a36716f9
Finley <finley.harrison2019@gmail.com>
2026-02-07 16:59:07 -0500

    server update

--- back-end/index.js
+++ back-end/index.js
@@ -252,4 +252,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
