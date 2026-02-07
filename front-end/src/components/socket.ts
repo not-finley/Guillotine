dc725f3767ae3bc1fac4ad8c884dc61ab1d685a0
Finley <finley.harrison2019@gmail.com>
2026-02-07 16:50:09 -0500

    logging update

--- front-end/src/components/socket.ts
+++ front-end/src/components/socket.ts
@@ -3,6 +3,8 @@ import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001";
 
+console.log(SOCKET_URL)
+
 const socket: Socket = io(SOCKET_URL, {
     transports: ["websocket"],
 });
